38e6e443f306aad299e4a3566e24be0a274cbc03
Alireza Doostimehr <alireza.doostimehr@sotoon.ir>
2023-02-07 16:29:26 +0330

    added 01 exercise

---
+++ Basics/1/01.py
@@ -0,0 +1,7 @@
+# Write a NumPy program to get the numpy
+# version and show numpy build configuration.
+
+import numpy as np
+
+print("Numpy Version:", np.__version__)
+print("Numpy Build Configuration:", np.show_config())
